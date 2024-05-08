4fb78aad1a8c2ad7236c8533eb5e1dbeffbe89dc
anguscheng2508 <72135270+anguscheng2508@users.noreply.github.com>
2024-05-08 18:28:05 +0100

    Add files via upload

---
+++ nn/include/neural_net.h
@@ -0,0 +1,25 @@
+// Copyright 2020-present pytorch-cpp Authors
+#pragma once
+
+#include <torch/torch.h>
+
+class NeuralNetImpl : public torch::nn::Module {
+ public:
+    NeuralNetImpl(int64_t input_size, int64_t hidden_size, int64_t num_classes);
+
+    torch::Tensor forward(torch::Tensor x);
+
+ private:
+    torch::nn::Linear fc1;
+    torch::nn::Linear fc3;
+    torch::nn::Linear fc4;
+    torch::nn::Linear fc5;
+    torch::nn::Linear fc6;
+//    torch::nn::Linear fc7;
+//    torch::nn::Linear fc8;
+//    torch::nn::Linear fc9;
+//    torch::nn::Linear fc10;
+    torch::nn::Linear fc2;
+};
+
+TORCH_MODULE(NeuralNet);
